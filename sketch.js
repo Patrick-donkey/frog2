5f9a82303ba02776971ddd98bdb996e26aa7ff0a
Patrick-donkey <71304547+Patrick-donkey@users.noreply.github.com>
2020-12-01 13:23:01 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://patrick-donkey.github.io/frog2/model.json';
 
 
 function preload() {
